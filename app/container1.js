b1a395d450b8e06f6f27587ca4b463f23a7a31e1
Antimekumawat111 <aktank810@gmail.com>
2024-11-05 17:26:46 +0530

    first commit

---
+++ app/container1.js
@@ -0,0 +1,39 @@
+import SearchIcon from '@mui/icons-material/Search';
+import ProductionQuantityLimitsIcon from '@mui/icons-material/ProductionQuantityLimits';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+
+export default function SubHeader() {
+  return (
+    <div className="sub-header">
+      <div className="box">
+        <h1 id="shop">SHOP.CO</h1>
+        <div className="ancherTag">
+          <a href="#" className="tags" id='shopTag'>
+            Shop
+          </a>
+          <KeyboardArrowDownIcon id="downArrow" />
+          <a href="#" className="tags">
+            On Sale
+          </a>
+          <a href="#" className="tags">
+            New Arrivals
+          </a>
+          <a href="#" className="tags">
+            Brands
+          </a>
+        </div>
+      </div>
+      <div className="inputdiv">
+        <SearchIcon id="searchIcon" />
+        <input placeholder="Search for products..."  id="inputbox"/>
+      </div>
+    {/* <div className="rightSideIcons"> */}
+
+        <ProductionQuantityLimitsIcon id="cartIcon"/>
+        <PersonOutlineIcon id="profileIcon" />
+    {/* </div> */}
+
+    </div>
+  );
+}
---
+++ app/header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <>
+      <div className="page1">
+        <div className="header">
+          <h2>Sign up and get 20% off to your first order </h2>
+        </div>
+      </div>
+    </>
+  );
+}
