1114ec23404e3d97509f5d2e6324379dfbfd0611
Antimekumawat111 <aktank810@gmail.com>
2024-11-07 17:42:42 +0530

    nothint to commit

--- app/container1.js
+++ app/container1.js
@@ -1,7 +1,7 @@
-import SearchIcon from '@mui/icons-material/Search';
+// import SearchIcon from "@mui/icons-material/Search";
 import ProductionQuantityLimitsIcon from '@mui/icons-material/ProductionQuantityLimits';
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 export default function SubHeader() {
   return (
@@ -9,17 +9,17 @@ export default function SubHeader() {
       <div className="box">
         <h1 id="shop">SHOP.CO</h1>
         <div className="ancherTag">
-          <a href="#" className="tags" id="shopTag">Shop</a>
+          <a href="#" className="tags" id="shopTaag">Shop</a>
           <KeyboardArrowDownIcon id="downArrow" />
           <a href="#" className="tags">On Sale</a>
           <a href="#" className="tags">New Arrivals</a>
           <a href="#" className="tags">Brands</a>
         </div>
       </div>
-      <div className="inputdiv">
+      {/* <div className="inputdiv">
         <SearchIcon id="searchIcon" />
         <input placeholder="Search for products..." id="inputbox" />
-      </div>
+      </div> */}
       <ProductionQuantityLimitsIcon id="cartIcon" />
       <PersonOutlineIcon id="profileIcon" />
     </div>
---
+++ app/subHeader.js
@@ -0,0 +1,58 @@
+'use client';
+import { useState } from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import ProductionQuantityLimitsIcon from "@mui/icons-material/ProductionQuantityLimits";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+
+export default function SubHeader() {
+  return (
+    <div className="sub-header">
+      <div className="box">
+        <h1 id="shop">SHOP.CO</h1>
+        <div className="ancherTag">
+          <a href="#" className="tags" id="shopTaag">
+            Shop
+          </a>
+          <KeyboardArrowDownIcon id="downArrow" />
+          <a href="#" className="tags">
+            On Sale
+          </a>
+          <a href="#" className="tags">
+            New Arrivals
+          </a>
+          <a href="#" className="tags">
+            Brands
+          </a>
+        </div>
+      </div>
+      {/* <div className="inputdiv">
+        <SearchIcon id="searchIcon" />
+        <input placeholder="Search for products..." id="inputbox" />
+      </div> */}
+      {/* <Imp /> */}
+      <ProductionQuantityLimitsIcon id="cartIcon" />
+      <PersonOutlineIcon id="profileIcon" />
+    </div>
+  );
+}
+
+function Imp() {
+  const [name, setName] = useState("");
+
+  return (
+    <>
+      {/* <div className="inputdiv"> */}
+      {/* <SearchIcon id="searchIcon" /> */}
+
+      {/* <input placeholder="Search for products..." id="inputbox" /> */}
+
+      <input
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Enter your name"
+      />
+      {/* </div> */}
+    </>
+  );
+}
