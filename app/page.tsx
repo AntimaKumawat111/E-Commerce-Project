4ee3700c771b6940b3ee2960260caf92efa7994d
Antimekumawat111 <aktank810@gmail.com>
2024-11-07 21:22:07 +0530

    nothing

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+// import Header from "./Header";
+// import Header from "./Header";
 import Header from "./Header";
 // import SubHeader from "./Container1";
 import Rectangle2 from "../public/images/Rectangle2.png";
@@ -6,23 +8,6 @@ import Vector from "../public/images/Vector.png";
 import Vector2 from "../public/images/Vector2.png";
 import MainContainer from "./posterLeftContainer";
 import Btn from "./Butn";
-
-function Photo({ cardNo, id, imagesrc, pvalue, rate }: any) {
-  return (
-    <div className="cards" id={cardNo}>
-      <div className="card">
-        <img src={imagesrc} alt="product" className="card_img" id={id} />
-        <div className="card_info">
-          <a href="" target="_blank">
-            <p id="ptitle">{pvalue}</p>
-            <p id="pstars">stars</p>
-            <p id="prate">${rate}</p>
-          </a>
-        </div>
-      </div>
-    </div>
-  );
-}
 
 export default function Home() {
   return (
